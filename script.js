5718f52e9b94fd36a50d398ecd6a7307061f6d07
asukhorukova1 <asukhorukova@inbox.ru>
2017-11-24 08:56:37 +0100

    abstract first sketch

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function init() {
 
-	
+	function copyDiv() {
+	    var firstDivContent = document.getElementById('mydiv1');
+	    var secondDivContent = document.getElementById('mydiv2');
+	    secondDivContent.innerHTML = firstDivContent.innerHTML;
+	}
+	function scrolltop(){
+		    $('#mydiv1').find(".textbody").on('scroll', function () {
+
+	    	// console.log($(this).scrollTop());
+		    $('#mydiv2').find(".textbody").scrollTop($(this).scrollTop());
+
+		});
+		    
+	}
+	scrolltop();
+
+
+	//Call copyDiv on body="onload"
+	copyDiv();
+
 }
 
 
 window.onload = init();
+
