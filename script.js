ae94bc5e188aa9da3b88ce283bbcd363f1f26afc
asukhorukova1 <asukhorukova@inbox.ru>
2017-12-07 13:31:26 +0100

    separated functions with animations

--- script.js
+++ script.js
@@ -1,90 +1,113 @@
- function init() {
-// 	function printpage(){
-//     var originalContents = document.body.innerHTML;
-//     var printReport= document.getElementById('mydiv1').innerHTML;
-//     document.body.innerHTML = printReport;
-//     window.print();
-//     document.body.innerHTML = originalContents;
-// }
-// printpage();
-// function printpage()
-//    {
-//         var originalContents = document.body.innerHTML;
-//         var printReport= document.getElementById('results').innerHTML;
-//         document.body.innerHTML = printReport;
-//         window.print();
-//         document.body.innerHTML = originalContents;
-//     }
-//     $(".print").click(function(){
-//     	printpage();
-//     });
+var mydiv1w;
+var mydiv1h;
 
-	// $(".line").css("height","100%");
+
+
+function init() {
+
+	function centerdiv(){
+		var halfwidth = $("#mydiv1").width()/2;
+		$("#mydiv1").css("left","calc(50% - "+halfwidth+"px)");
+	}
+	
+
+
+
+
+	$.fn.isOnScreen = function(){
+
+		var win = $(window);
+
+		var viewport = {
+			top : win.scrollTop(),
+			left : win.scrollLeft()
+		};
+
+		viewport.right = viewport.left + win.width();
+		viewport.bottom = viewport.top + win.height();
+
+		var bounds = this.offset();
+		bounds.right = bounds.left + this.outerWidth();
+		bounds.bottom = bounds.top + this.outerHeight();
+
+		return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+	};
+
+	function resizewindow(){
+		centerdiv();
+		$("#mydiv1").css("width",mydiv1w);
+		$("#mydiv1").css("left","calc(50% - " + mydiv1w/2 + "px)");
+	}
+
+	function sidesnormal(){
+		$("#top, #bottom").css("height",($(window).height() - $("#mydiv1").height())/2);
+		$("#left, #right").css("width",($(window).width() - $("#mydiv1").width())/2);
+	}
+
+	function animation1(){
+		mydiv1w = $("#mydiv1").height()*1.6;
+		resizewindow();
+		sidesnormal();	
+	}
+
+	function animation2(){
+		mydiv1w = $("#mydiv1").height()*1.5;
+		resizewindow();
+		sidesnormal();
+
+	}
+
+	function animation3(){
+		$("#left, #right").css("width","100%");
+	}
+
+	$(".canvas").css("height",$("#mydiv1").height()*2);
 
 	function copyDiv() {
-	    var firstDivContent = document.getElementById('mydiv1');
-	    var secondDivContent = document.getElementById('mydiv2');
-	    secondDivContent.innerHTML = firstDivContent.innerHTML;
+		var firstDivContent = document.getElementById('mydiv1');
+		var secondDivContent = document.getElementById('mydiv2');
+		secondDivContent.innerHTML = firstDivContent.innerHTML;
 	}
+
 	function scrolltop(){
-		var change1;
-		var change2;
-		var changewidth;
-		
-		    $('#mydiv1').find(".textbody").on('scroll', function () {
-		    change1 = $("#mydiv1").find("h2").offset().top ;
-		    change2 = $("anchor").offset().top + 1000;
-		    var change3 ;
-		    console.log(change1);
-		    $('#mydiv2').find(".textbody").scrollTop($(this).scrollTop());
-            
-            
 
-		
-			if($("#mydiv1").find(".textbody").scrollTop() < change1){
-				// changewidth = $("#mydiv1").height()*1.8;
-				$("#mydiv1").css("width","65%");
-				$("#mydiv1").css("left","17.5%");
-				$("#top, #bottom").css("height",($(window).height() - $("#mydiv1").height())/2);
-		    	$("#left, #right").css("width",($(window).width() - $("#mydiv1").width())/2);
-		    	console.log($(".canvas").offset().top);
 
+		var canvas = $("#mydiv1").find("img");
+		var canvas2 = $("#mydiv1").find("p");
+		var canvas3 = $("#mydiv1").find("#canvas2");
+		
+		$('#mydiv1').find(".textbody").on('scroll', function () {
+
+
+			$('#mydiv2').find(".textbody").scrollTop($(this).scrollTop());
+
+
+			if (canvas.isOnScreen() === true){
+				animation1()
+
+			}else if(canvas2.isOnScreen() === true){
+				animation2();
+
+			}else if(canvas3.isOnScreen() === true){
+				animation3();
+
+			}else{
+				sidesnormal();
 				
-				// $("#mydiv1").css("border-radius","100%");
-				// $("#mydiv1").find(".textbody").css("border-radius","100%");
-				// $("#textbody").find("img").css("margin-left"," - "+ $("img").width()/1 +"px")
-				// changewidth = $("#mydiv1").height();
-				
-			}else if($("#mydiv1").find(".textbody").scrollTop() > change1 && $("#mydiv1").find(".textbody").scrollTop() < change2){
-				$("#mydiv1").css("width",changewidth);
-			    $("#mydiv1").css("left","calc(50% - "+changewidth/2+"px)");
-				$("#mydiv1").css("border-radius","0%");
-				$("#mydiv1").find(".textbody").css("border-radius","0%");
-				changewidth = $("#mydiv1").height()*1.5;
-				$("#top, #bottom").css("height",($(window).height() - $("#mydiv1").height())/2);
-		    	$("#left, #right").css("width",($(window).width() - $("#mydiv1").width())/2);
-			}else if($(".canvas").offset().top > 100){
-				$("#left, #right").css("width","100%");
-				// $(".circle").css("left","calc(-50% - "+39+"px)")
 			}
 
-			});
+		});
 
 	}
-	function changeshape(){
-			// $("#top, #bottom").css("height",($(window).height() - $("#mydiv1").height())/2);
-		 //    $("#left, #right").css("width",($(window).width() - $("#mydiv1").width())/2);
 
-	}
-	changeshape()
-	
 
 	copyDiv();
 	scrolltop();
 	setInterval(function(){ 
 				// $("#top, #bottom").css("height",($(window).height() - $("#mydiv1").height())/2);
 		  //   	$("#left, #right").css("width",($(window).width() - $("#mydiv1").width())/2);
-	 }, 1);
+		}, 1);
 
 }
 
